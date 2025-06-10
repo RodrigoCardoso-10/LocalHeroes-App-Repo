2abbd8ae35ebe38a8823770c48f7f775e2e0372b
Rodrigo <rodricardoso7@outlook.com>
2025-06-10 14:58:45 +0200

    Reorganised the file structure and working on adding  bottom navigation bar on the pages

---
+++ local-heroes/app/(tabs)/post.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function PostScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Post a Job Screen</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+}); 
